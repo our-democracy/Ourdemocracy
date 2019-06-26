67c33674a53b8f59a77d048cf9822a6931e038c9
shakir <shakirl88@gmail.com>
2019-06-26 21:38:05 +0530

    static root changes

--- newproject/newproject/settings.py
+++ newproject/newproject/settings.py
@@ -87,7 +87,7 @@ WSGI_APPLICATION = 'newproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'django_boards',
         'USER': 'u_boards',
         'PASSWORD': 'root',
@@ -134,7 +134,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
